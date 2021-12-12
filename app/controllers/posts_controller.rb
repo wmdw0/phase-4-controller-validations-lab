65644bccb4b77dbc7c9809f1e1d07e728da7239f
William Walsh <wmdwalsh0@gmail.com>
2021-12-12 18:24:41 -0500

    commit tree 6baff9f8e25e03337d0f02ce24c0a6a87901ab4b

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   def show
     post = Post.find(params[:id])
     
@@ -7,17 +8,28 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
+      post = find_post
+      # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+      post.update!(post_params)
+      render json: post
   end
 
   private
 
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
+  def find_post
+    Post.find(params[:id])
+  end
+
   def post_params
-    params.permit(:category, :content, :title)
+    params.permit(:title, :category, :content)
+  end
+
+  def render_not_found_response
+    render json: { error: "Post not found" }, status: :not_found
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, inclusion: { in: (["Fiction", "Non-Fiction"]),
+    message: "is not included in the list" }
+    validates :content, length: { minimum: 100 }
 end
