15f6531b5f261f09c2ddcaa9dc59061bc4f5ebbd
William Walsh <wmdwalsh0@gmail.com>
2021-12-12 18:24:54 -0500

    completed assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,7 @@
 class AuthorsController < ApplicationController
-  
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def show
     author = Author.find(params[:id])
 
@@ -7,15 +9,25 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+    # author = Author.create(author_params)
+
+    # render json: author, status: :created
   end
 
   private
   
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
   def author_params
-    params.permit(:email, :name)
+    params.permit(:name, :email)
+  end
+
+  def render_not_found_response
+    render json: { error: "Post not found" }, status: :not_found
   end
   
 end
